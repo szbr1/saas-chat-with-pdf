af216c48a4f767e78d59be91fc36f5c89ff11c19
szbr1 <szb8480@gmail.com>
2025-07-16 11:40:47 +0500

    feat: fix remove openAi causing issue and integrate huggyface api

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,13 +1,16 @@
 import { put } from '@vercel/blob';
-
+// we are using vercel blob storage  || ✅vercel blob storage ||❌ firebase storage ||❌ cloudinary storage
 export const POST = async (req) => {
+
+  // getting the fiel from the request 
   const formData = await req.formData();
   const file = formData.get('file');
-
+ 
+  // file check available or not 
   if (!file) {
     return new Response(JSON.stringify({ error: 'No file found' }), { status: 400 });
   }
-
+  // this will send to the vercel blob storage
   const blob = await put(file.name, file, {
     access: 'public',
     token: process.env.VERCEL_BLOB_READ_WRITE_TOKEN, // ✅ use token here
